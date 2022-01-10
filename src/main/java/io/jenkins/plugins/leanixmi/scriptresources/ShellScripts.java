dffcd3fa77c296769a5d7a103816f188727f26b6
Keerthan22 <keerthan.avula@leanix.net>
2022-01-10 11:28:52 +0530

    CID-478: Include condition to check the path of mavenSettingsFile and update the dependency generation command accordingly

--- src/main/java/io/jenkins/plugins/leanixmi/scriptresources/ShellScripts.java
+++ src/main/java/io/jenkins/plugins/leanixmi/scriptresources/ShellScripts.java
@@ -20,25 +20,32 @@ public class ShellScripts {
             "gradle generateLicenseReport -I %gradleInitFile%\n" +
             ")";
 
-    public static final String shellScript = "#!/bin/bash\n" +
-            "export pathToDependencyDir=$1;\n" +
-            "export packageManager=$2;\n" +
-            "export gradleInitFile=$3;\n" +
-            "\n" +
-            "case $packageManager in\n" +
-            "  'NPM')\n" +
-            "    cd $pathToDependencyDir;\n" +
-            "    npm install -g license-checker;\n" +
-            "    npm install;\n" +
-            "    license-checker --json > $pathToDependencyDir/dependencies.json;\n" +
-            "    ;;\n" +
-            "  'MAVEN')\n" +
-            "    cd $pathToDependencyDir;\n" +
-            "    mvn org.codehaus.mojo:license-maven-plugin:download-licenses;\n" +
-            "    ;;\n" +
-            "  'GRADLE')\n" +
-            "    cd $pathToDependencyDir;\n" +
-            "    gradle generateLicenseReport -I $gradleInitFile;\n" +
-            "    ;;\n" +
-            "esac";
+    public static final String shellScript = "#!/bin/bash\n"
+        + "export pathToDependencyDir=$1;\n"
+        + "export packageManager=$2;\n"
+        + "export gradleInitFile=$3;\n"
+        + "export mavenSettingsFile=$4;\n"
+        + "\n"
+        + "case $packageManager in\n"
+        + "  'NPM')\n"
+        + "    cd $pathToDependencyDir;\n"
+        + "    npm install -g license-checker;\n"
+        + "    npm install;\n"
+        + "    license-checker --json > $pathToDependencyDir/dependencies.json;\n"
+        + "    ;;\n"
+        + "  'MAVEN')\n"
+        + "    cd $pathToDependencyDir;\n"
+        + "    \n"
+        + "    if [[ $mavenSettingsFile != \"\" ]]; then\n"
+        + "      echo \"Maven repository detected with custom user settings (using path $mavenSettingsFile). Attempting to generate dependency file\"\n"
+        + "      mvn -s $mavenSettingsFile org.codehaus.mojo:license-maven-plugin:download-licenses;\n"
+        + "    else\n"
+        + "      echo \"Maven repository detected. Attempting to generate dependency file\"\n"
+        + "      mvn org.codehaus.mojo:license-maven-plugin:download-licenses;\n"
+        + "    ;;\n"
+        + "  'GRADLE')\n"
+        + "    cd $pathToDependencyDir;\n"
+        + "    gradle generateLicenseReport -I $gradleInitFile;\n"
+        + "    ;;\n"
+        + "esac";
 }
