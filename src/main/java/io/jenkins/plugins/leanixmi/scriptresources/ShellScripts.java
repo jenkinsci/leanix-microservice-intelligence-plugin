74435091919d6880120993db27a606ad991ea4ce
Keerthan22 <keerthan.avula@leanix.net>
2022-01-10 11:52:07 +0530

    CID-478: Update the batch script to include maven settings path

--- src/main/java/io/jenkins/plugins/leanixmi/scriptresources/ShellScripts.java
+++ src/main/java/io/jenkins/plugins/leanixmi/scriptresources/ShellScripts.java
@@ -2,23 +2,24 @@ package io.jenkins.plugins.leanixmi.scriptresources;
 
 public class ShellScripts {
 
-    public static final String batchScriptWin = "set pathToDependencyDir=%1\n" +
-            "set packageManager=%2\n" +
-            "set gradleInitFile=%3%\n" +
-            "if %packageManager%==NPM (\n" +
-            "cd %pathToDependencyDir%\n" +
-            "npm install -g license-checker\n" +
-            "npm install\n" +
-            "license-checker --json > %pathToDependencyDir%/dependencies.json\n" +
-            ")\n" +
-            "if %packageManager%==MAVEN (\n" +
-            "cd %pathToDependencyDir%\n" +
-            "mvn org.codehaus.mojo:license-maven-plugin:download-licenses\n" +
-            ")\n" +
-            "if %packageManager%==GRADLE (\n" +
-            "cd %pathToDependencyDir%\n" +
-            "gradle generateLicenseReport -I %gradleInitFile%\n" +
-            ")";
+    public static final String batchScriptWin = "set pathToDependencyDir=%1\n"
+        + "set packageManager=%2\n"
+        + "set gradleInitFileOrMavenSettingsFile=%3%\n"
+        + "if %packageManager%==NPM (\n"
+        + "cd %pathToDependencyDir%\n"
+        + "npm install -g license-checker\n"
+        + "npm install\n"
+        + "license-checker --json > %pathToDependencyDir%/dependencies.json\n"
+        + ")\n"
+        + "if %packageManager%==MAVEN (\n"
+        + "cd %pathToDependencyDir%\n"
+        + "if %gradleInitFileOrMavenSettingsFile%!=\"\" (  mvn -s %gradleInitFileOrMavenSettingsFile% org.codehaus.mojo:license-maven-plugin:download-licenses ) \n"
+        + "else ( mvn org.codehaus.mojo:license-maven-plugin:download-licenses ) \n"
+        + ")\n"
+        + "if %packageManager%==GRADLE (\n"
+        + "cd %pathToDependencyDir%\n"
+        + "gradle generateLicenseReport -I %gradleInitFileOrMavenSettingsFile%\n"
+        + ")";
 
     public static final String shellScript = "#!/bin/bash\n"
         + "export pathToDependencyDir=$1;\n"
@@ -44,7 +45,7 @@ public class ShellScripts {
         + "    ;;\n"
         + "  'GRADLE')\n"
         + "    cd $pathToDependencyDir;\n"
-        + "    gradle generateLicenseReport -I $gradleInitFile;\n"
+        + "    gradle generateLicenseReport -I gradleInitFileOrMavenSettingsFile;\n"
         + "    ;;\n"
         + "esac";
 }
